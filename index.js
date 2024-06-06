6b3efdb53b131e37457e58563dd52c6ae0f134b6
wcheung021 <wcheung021@gmail.com>
2024-06-06 16:46:05 -0500

    mongo connection fixed

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const Users = Models.User;
 app.use(bodyParser.json());
 app.use(morgan("common"));
 
-mongoose.connect('mongodb://localhost:27017/123', { useUnifiedTopology: true });
+mongoose.connect('mongodb://127.0.0.1:27017/123', { useUnifiedTopology: true });
 
 mongoose.connection.on('connected', () => {
   console.log('Connected to MongoDB');
