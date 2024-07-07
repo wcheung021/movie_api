294e944962a9ff5d4067fafaed240e34614f3323
wcheung021 <wcheung021@gmail.com>
2024-07-07 16:36:45 -0500

    v0.02

--- models.js
+++ models.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 let movieSchema = mongoose.Schema({
     Title: {type: String, required: true},
